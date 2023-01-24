fc460c79ec582d4fb62bd09a0a97a30dfcb3bbad
Jonathanbir <jonathan79111@gmail.com>
2023-01-25 00:26:21 +0800

    commit: GET 學生資料

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Student = require("./models/student");
+
 let port = 3000;
 
 app.set("view engine", "ejs");
@@ -13,6 +15,27 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
+
+app.get("/students", async (req, res) => {
+  try {
+    let studentData = await Student.find({}).exec();
+    return res.send(studentData);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤...");
+  }
+});
+
+app.get("/students/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundStudent = await Student.find({
+      _id,
+    }).exec();
+    return res.send(foundStudent);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤...");
+  }
+});
 
 app.listen(port, () => {
   console.log("伺服器正在聆聽" + port + "...");
---
+++ models/student.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const studentSchema = new Schema({
+  name: { type: String, required: true, minLength: 1, maxLength: 25 },
+  age: { type: Number, min: [0, "年齡不能小於0"] },
+  //   major: { type: String, required: [true, "每個學生都需要選至少一個主修"] },
+  major: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  age: {
+    type: Number,
+    default: 18,
+    max: [80, "可能有點太老了..."],
+  },
+  scholarship: {
+    merit: {
+      type: Number,
+      min: 0,
+      max: [5000, "學生merit scholarship 也太多了"],
+      default: 0,
+    },
+    other: { type: Number, min: 0, default: 0 },
+  },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
